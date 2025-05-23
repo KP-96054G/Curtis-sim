22d881e30991085aa58b16f258645216de707b85
KP-96054G <keshbpatel19@gmail.com>
2025-05-23 17:24:15 -0500

    Update teacher.java

--- teacher.java
+++ teacher.java
@@ -1,1 +1,38 @@
-
+public class teacher {
+    
+    private String name;
+    private int day;
+    private int score;
+    
+    public teacher(){
+        name = "Curtis";
+        day = 0;
+        score = 100;
+    }
+    
+    public void advanceDay(){
+        day++;
+    }
+    
+    public void startDay(){
+        System.out.println("Day: " + day);
+    }
+    
+    public void event(){
+        int choose = (int)(Math.random() *10) + 1;
+        //System.out.println(choose);
+        event er = new event();
+        score -= er.runEvent(choose);
+        
+    }
+    
+    public void printScore(){
+        System.out.println("Score: " + score);
+    }
+    
+    public int getScore(){
+        return score;
+    }
+    
+     
+}
