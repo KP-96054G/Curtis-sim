27ac079cac3845dca67d1b3bf89ac99b862b389b
KP-96054G <keshbpatel19@gmail.com>
2025-05-23 17:25:41 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,30 @@
-
+import java.util.Scanner;
+public class MyProgram
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        teacher curt = new teacher();
+        System.out.println("Welcome to Curtis Sim!");
+        while(curt.getScore() > 0){
+            runDay(curt);
+        }
+        
+        System.out.println("\nYOU'VE BEEN FIRED");
+        
+        
+        
+        
+    }
+    
+    public static void runDay(teacher curt){
+        Scanner nxt = new Scanner(System.in);
+        System.out.println("\nPress enter to advance day");
+        String adv = nxt.nextLine();
+        curt.advanceDay();
+        curt.startDay();
+        curt.event();
+        curt.printScore();
+        
+    }
+}
